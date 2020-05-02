a81cc34a0a711e1695c7097e9b1cd460835d37d5
Marcos Cerqueira <marcos.cerqueira@linx.com.br>
2020-05-01 23:49:23 -0300

    Add computedProperty, e criando uma function para filtar nossa lista

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,10 @@
   <div class="corpo">
     <h1 class="centralizado">{{ titulo }}</h1>
 
+    <input type="search" class="filtro" v-on:input="filtro = $event.target.value" placeholder="filtre pelo título da foto">
+
     <ul class="lista-fotos">
-      <li class="lista-fotos-item" v-for="foto of fotos" :key="foto.id">
+      <li class="lista-fotos-item" v-for="foto of fotosComFiltro" :key="foto.id">
         <meu-painel :titulo="foto.titulo">
           <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo" />
         </meu-painel>
@@ -23,9 +25,26 @@
     data() {
       return {
         titulo: 'Alurapic',
-        fotos: []
+        fotos: [],
+        filtro: ''
       }
     },
+
+    computed: {
+      fotosComFiltro() {
+        if(this.filtro) {
+          // criando uma expressão com o valor do filtro, insensitivo
+          let exp = new RegExp(this.filtro.trim(), 'i');
+
+          // retorna apenas as fotos que condizem com a expressão
+          return this.fotos.filter(foto => exp.test(foto.titulo)); 
+        }
+
+        // se o campo estiver vazio, não filtramos, retornamos a lista
+        return this.fotos;
+      }
+    },
+
     created() {
       this.$http.get('http://localhost:3000/v1/fotos')
         // .then(res => console.log(res))
@@ -56,6 +75,11 @@
   }
 
   .imagem-responsiva {
+    width: 100%;
+  }
+
+  .filtro {
+    display: block;
     width: 100%;
   }
 </style>
