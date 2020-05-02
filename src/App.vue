96f65af4fedecb572921569289fc9d7ae930de4a
Marcos Cerqueira <marcos.cerqueira@linx.com.br>
2020-05-01 22:27:50 -0300

    Add styles

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <template>
-  <div>
-    <h1>{{ titulo }}</h1>
+  <div class="corpo">
+    <h1 class="centralizado">{{ titulo }}</h1>
 
-    <ul>
-      <li v-for="foto of fotos" :key="foto.id">
-        <img :src="foto.url" :alt="foto.titulo" />
-
-        <p v-text="foto.titulo"></p>
+    <ul class="lista-fotos">
+      <li class="lista-fotos-item" v-for="foto of fotos" :key="foto.id">
+        <div class="painel">
+          <h2 class="painel-titulo" v-text="foto.titulo"></h2>
+          
+          <div class="painel-corpo">
+            <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo" />
+          </div>
+        </div>
       </li>
     </ul>
   </div>
@@ -30,5 +34,47 @@ export default {
 </script>
 
 <style>
+  .corpo{
+    font-family: Helvetica, sans-serif;
+    width: 96%;
+    margin: 0 auto;
+  }
 
+  .centralizado {
+    text-align: center;
+  }
+
+  .lista-fotos {
+    list-style: none;
+    padding: 0;
+  }
+
+  .lista-fotos .lista-fotos-item {
+    display: inline-block;
+  }
+
+  .imagem-responsiva {
+    width: 100%;
+  }
+
+  .painel {
+    padding: 0 auto;
+    border: solid 2px grey;
+    display: inline-block;
+    margin: 5px;
+    box-shadow: 5px 5px 10px grey;
+    width: 200px;
+    height: 100%;
+    vertical-align: top;
+    text-align: center;
+  }
+
+  .painel .painel-titulo {
+    text-align: center;
+    border: solid 2px;
+    background: lightblue;
+    margin: 0 0 15px 0;
+    padding: 10px;
+    text-transform: uppercase;
+  }
 </style>
