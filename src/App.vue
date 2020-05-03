3c4fa5764976507c414fe62f2b44082f03666e9a
Marcos Cerqueira <marcos.cerqueira@linx.com.br>
2020-05-02 22:17:32 -0300

    Add menu for routes

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,26 @@
 <template>
   <div class="corpo">
+    <nav>
+      <ul>       
+        <li v-for="route in routes" :key="route.titulo">
+          <router-link :to="route.path ? route.path : '/'">{{ route.titulo }}</router-link>
+        </li>
+      </ul>
+    </nav>
+
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+  import { routes } from './routes';
+
   export default {
-    
+    data() {
+      return {
+        routes
+      }
+    }
   }
 </script>
 
--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,6 @@ import Home from './components/home/Home';
 import Cadastro from './components/cadastro/Cadastro';
 
 export const routes = [
-  { path: '', component: Home },
-  { path: '/cadastro', component: Cadastro }
+  { path: '', component: Home, titulo: 'Home' },
+  { path: '/cadastro', component: Cadastro, titulo: 'Cadastro' }
 ]
