73e4ce11ca2ab7fe6cc385e4aad47528d4da50bb
Marcos Cerqueira <marcos.cerqueira@linx.com.br>
2020-05-02 21:02:58 -0300

    NewComponent Cadastro

---
+++ src/components/cadastro/Cadastro.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>{{ title }}</h1>
+</template>
+
+<script>
+    export default {
+      data() {
+        return {
+          title: 'Cadastro'
+        }
+      }
+    }
+</script>
+
+<style lang="stylus">
+  .centralizado { text-align: center; }
+</style>
